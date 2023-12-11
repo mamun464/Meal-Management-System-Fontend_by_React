21065b62286e6d685f30cc99d5c6e9f57ffc3683
Mamun <mamunurrashid.s.bd@gmail.com>
2023-12-11 18:36:05 +0600

    Invoice dropdown create

--- src/Components/Inventory/Invoice/Invoice.jsx
+++ src/Components/Inventory/Invoice/Invoice.jsx
@@ -1,9 +1,118 @@
-import React, { useState } from "react";
+import { useEffect, useState } from "react";
 import DatePicker from "react-datepicker";
 import "react-datepicker/dist/react-datepicker.css";
+import { MdLibraryAdd } from "react-icons/md";
+import InvoiceDropdown from "./InvoiceDropDown/InvoiceItemDropdown";
+import base_url from "../../../../public/config";
+import InvoiceVariantdropDown from "./InvoiceDropDown/InvoiceVariantdropDown";
 
 const Invoice = () => {
+    const [itemDropdown, setItemDropdown] = useState([]);
+    const [category, SetCategory] = useState('');
+    const [VariantID, SetVariantID] = useState(0);
+    const [filterVariant, SetFilterVariant] = useState([]);
+
     const [startDate, setStartDate] = useState(new Date());
+    const [items, setItems] = useState([{ itemType: "", variant: "", quantity: "", unitRate: "", amount: "" }]);
+
+    const handleAddItem = () => {
+        setItems([...items, { itemType: "", variant: "", quantity: "", unitRate: "", amount: "" }]);
+    };
+
+    const handleItemChange = (index, field, value) => {
+        const updatedItems = [...items];
+        updatedItems[index][field] = value;
+        setItems(updatedItems);
+    };
+
+    const handleNumericFieldChange = (index, field, value) => {
+        const updatedItems = [...items];
+        updatedItems[index][field] = parseFloat(value) || 0; // Parse value as a float, default to 0 if not a valid number
+        setItems(updatedItems);
+    };
+
+    const handleSubmit = () => {
+        // Collect and submit the data wherever needed
+        const formData = {
+            date: startDate,
+            billTo: "", // Add the actual value from the "Bill to" textarea
+            shipTo: "", // Add the actual value from the "Ship to" textarea
+            items: [...items],
+        };
+
+        // Example: Log the form data to the console
+        console.log(formData);
+    };
+
+
+
+    const itemListFetch = async (url) => {
+        try {
+            const response = await fetch(`${base_url}${url}`);
+
+            if (!response.ok) {
+                throw new Error(`HTTP error! Status: ${response.status}`);
+            }
+
+            const result = await response.json();
+
+            setItemDropdown(result.unique_item_names);
+            // console.log("Item Fetch+++++++Inside++++++++++:", itemDropdown);
+        } catch (error) {
+            console.error('Error fetching data:', error);
+        }
+    };
+
+    useEffect(() => {
+        // Now you can use itemListFetch here
+        itemListFetch('/api/inventory/unique-item-names/');
+    }, []);
+
+    useEffect(() => {
+        // Now you can use itemListFetch here
+
+    }, [itemDropdown]);
+
+
+
+    useEffect(() => {
+
+        const fetchCategoryData = async () => {
+            // console.log('Fetching category data', category);
+            try {
+                const response = await fetch(`${base_url}/api/inventory/get-item-variant/?item_name=${category}`);
+
+                if (!response.ok) {
+                    throw new Error(`HTTP error! Status: ${response.status}`);
+                }
+
+                const result = await response.json();
+                SetFilterVariant(result);
+            } catch (error) {
+                console.error('Error fetching data:', error);
+            }
+        };
+        fetchCategoryData();
+
+
+    }, [category]);
+
+    const refreshDropdown = () => {
+        itemListFetch('/api/inventory/unique-item-names/');
+    }
+
+    const handleItem = (categoryName) => {
+
+        SetCategory(categoryName)
+    }
+
+    const handleVariant = (variantId) => {
+        SetVariantID(variantId)
+
+    }
+
+
+
     return (
         <div className="max-w-7xl mx-auto border p-4 rounded-md bg-gray-50 drop-shadow-lg">
             <h1 className="uppercase text-3xl text-[#233255CC] font-bold text-center my-3">Invoice</h1>
@@ -23,7 +132,6 @@ const Invoice = () => {
                         <h1 className="uppercase text-[15px] text-[#233255CC] font-bold mb-1">Date</h1>
                         <DatePicker className="border border-gray-300 p-2 rounded-md" selected={startDate} onChange={(date) => setStartDate(date)} />
                     </div>
-
                     <div className="flex items-center gap-3">
                         <h1 className="uppercase text-[15px] text-[#233255CC] font-bold mb-1">PO Number</h1>
                         <input type="tel" className="border border-gray-300 p-2 rounded-md" placeholder="PO Number" pattern="[0-9]*" />
@@ -31,38 +139,80 @@ const Invoice = () => {
                 </div>
             </div>
 
-            <table className="border-collapse w-full mb-4 mt-16">
-                <thead>
-                    <tr className="border-2 rounded-xl bg-gray-300 text-left">
-                        <th className="border-b-2 px-4 py-2">Item</th>
-                        <th className="border-b-2 px-4 py-2">Variant</th>
-                        <th className="border-b-2 px-4 py-2">Quantity</th>
-                        <th className="border-b-2 px-4 py-2">Rate</th>
-                        <th className="border-b-2 px-4 py-2">Amount</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    <tr>
-                        <td className="border-b-2 px-4 py-2">Beef</td>
-                        <td className="border-b-2 px-4 py-2">Deshi</td>
-                        <td className="border-b-2 px-4 py-2">1</td>
-                        <td className="border-b-2 px-4 py-2">$800</td>
-                        <td className="border-b-2 px-4 py-2">$800.00</td>
-                    </tr>
+            <div className="flex bg-[#242e38] uppercase text-sm text-[#fff] font-bold p-2 rounded-md mt-5">
+                <div className="flex" style={{ flex: '7' }}>
+                    <div className="flex-1">
+                        <h1>Item</h1>
+                    </div>
+                    <div className="flex-1">
+                        <h1>Variant</h1>
+                    </div>
+                </div>
+                <div className="flex gap-3 justify-between" style={{ flex: '3' }}>
+                    <h1>Quantity</h1>
+                    <h1>Unit Rate</h1>
+                    <h1>Amount</h1>
+                </div>
+            </div>
 
-                </tbody>
-                <tfoot>
-                    <tr>
-                        <th className="border-t-2 px-4 py-2" colSpan="3">
-                            Subtotal
-                        </th>
-                        <th className="border-t-2 px-4 py-2">$1,000.00</th>
-                    </tr>
-                </tfoot>
-            </table>
+            {items.map((item, index) => (
+                <div key={index} className="my-2 flex">
+                    <div className="flex gap-2" style={{ flex: '7' }}>
+                        <div className="flex-1">
+                            <InvoiceDropdown
+                                type="Item"
+                                value={item.itemType}
+                                filterVariant={filterVariant}
+                                refreshDropdown={refreshDropdown}
+                                handleItem={handleItem}
+                                itemListFetch={itemListFetch}
+                                onChange={(e) => handleItemChange(index, 'itemType', e.target.value)}
+                            />
+                        </div>
+                        <div className="flex-1">
+                            <InvoiceVariantdropDown
+                                type="Variant"
+                                value={item.variant}
+                                filterVariant={filterVariant}
+                                onChange={(e) => handleItemChange(index, 'variant', e.target.value)}
+                            />
+                        </div>
+                    </div>
+                    <div className="flex gap-2 justify-between pl-4" style={{ flex: '3' }}>
+                        <input
+                            className="border border-gray-300 p-2 w-full rounded-lg"
+                            type="number"
+                            value={item.quantity}
+                            onChange={(e) => handleNumericFieldChange(index, 'quantity', e.target.value)}
+                        />
+                        <input
+                            className="border border-gray-300 p-2 w-full rounded-lg"
+                            type="number"
+                            value={item.unitRate}
+                            onChange={(e) => handleNumericFieldChange(index, 'unitRate', e.target.value)}
+                        />
+                        <input
+                            className="border border-gray-300 p-2 w-full rounded-lg"
+                            type="tel"
+                            value={item.amount}
+                            onChange={(e) => handleNumericFieldChange(index, 'amount', e.target.value)}
+                        />
+                    </div>
+                </div>
+            ))}
 
+            <div>
+                <button className="btn btn-accent text-white bg-[#009f6f] font-bold uppercase" onClick={handleAddItem}>
+                    <span>
+                        <MdLibraryAdd />
+                    </span>Item
+                </button>
+
+                <button className="btn btn-accent text-white bg-[#009f6f] font-bold uppercase" onClick={handleSubmit}>
+                    Submit
+                </button>
+            </div>
         </div>
-
     );
 };
 
---
+++ src/Components/Inventory/Invoice/InvoiceDropDown/InvoiceItemDropdown.jsx
@@ -0,0 +1,64 @@
+import PropTypes from 'prop-types';
+import { useState } from 'react';
+import {
+    TEDropdown,
+    TEDropdownToggle,
+    TEDropdownMenu,
+    TEDropdownItem,
+    TERipple,
+} from "tw-elements-react";
+
+const InvoiceItemDropdown = ({ itemDropdown, refreshDropdown, handleItem }) => {
+    // console.log("sInvoiceDropdown-------->", itemDropdown)
+    const [type, SetType] = useState('Select Item')
+    const handleItemClick = (item) => {
+        handleItem(item)
+        SetType(item)
+    };
+    return (
+        <TEDropdown className="flex w-full" onClick={refreshDropdown}>
+            <TERipple rippleColor="light" className="w-full">
+                <TEDropdownToggle className="flex w-full text-[#233255CC] font-bold items-center justify-center whitespace-nowrap rounded bg-white px-6 p-2.5 text-xs  uppercase leading-normal text-black border border-gray-300 shadow-[0_4px_9px_-4px_rgba(0,0,0,0.1)] transition duration-150 ease-in-out hover:bg-gray-100 hover:shadow-[0_8px_9px_-4px_gray-400,0_4px_18px_0_gray-300] focus:bg-gray-100 focus:shadow-[0_8px_9px_-4px_gray-400,0_4px_18px_0_gray-300] focus:outline-none focus:ring-0 active:bg-gray-200 active:shadow-[0_8px_9px_-4px_gray-400,0_4px_18px_0_gray-300] motion-reduce:transition-none dark:shadow-[0_4px_9px_-4px_gray-500] dark:hover:shadow-[0_8px_9px_-4px_gray-400,0_4px_18px_0_gray-500] dark:focus:shadow-[0_8px_9px_-4px_gray-400,0_4px_18px_0_gray-500] dark:active:shadow-[0_8px_9px_-4px_gray-400,0_4px_18px_0_gray-500]">
+                    {type}
+                    <span className="ml-2 [&>svg]:w-5 w-2">
+                        <svg
+                            xmlns="http://www.w3.org/2000/svg"
+                            viewBox="0 0 20 20"
+                            fill="currentColor"
+                        >
+                            <path
+                                fillRule="evenodd"
+                                d="M5.23 7.21a.75.75 0 011.06.02L10 11.168l3.71-3.938a.75.75 0 111.08 1.04l-4.25 4.5a.75.75 0 01-1.08 0l-4.25-4.5a.75.75 0 01.02-1.06z"
+                                clipRule="evenodd"
+                            />
+                        </svg>
+                    </span>
+                </TEDropdownToggle>
+            </TERipple>
+
+            <TEDropdownMenu>
+                <ul className="list-none p-0 m-0">
+                    {itemDropdown.map((item, index) => (
+                        <TEDropdownItem key={index}>
+                            <li onClick={() => {
+                                handleItemClick(item);
+                            }} className="block w-full min-w-[160px] cursor-pointer whitespace-nowrap bg-transparent px-4 py-2 text-sm text-left font-normal pointer-events-auto text-neutral-700 hover:bg-neutral-100 active:text-neutral-800 active:bg-neutral-100 focus:bg-neutral-100 focus:text-neutral-800 focus:outline-none active:no-underline dark:text-neutral-200 dark:hover:bg-neutral-600 dark:focus:bg-neutral-600 dark:active:bg-neutral-600">
+                                {item}
+                            </li>
+                        </TEDropdownItem>
+                    ))}
+                </ul>
+            </TEDropdownMenu>
+        </TEDropdown>
+    );
+};
+
+export default InvoiceItemDropdown;
+
+InvoiceItemDropdown.propTypes = {
+
+    itemDropdown: PropTypes.array.isRequired,
+    handleItem: PropTypes.func.isRequired,
+    refreshDropdown: PropTypes.func.isRequired,
+    type: PropTypes.string.isRequired,
+}
